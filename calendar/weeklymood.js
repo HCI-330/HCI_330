b921871f7e9e04e71dde8eeed5ee220ad43481ad
zezhu2022 <zezhu2022@u.northwestern.edu>
2020-03-14 18:50:12 -0500

    added axis titles

--- calendar/weeklymood.js
+++ calendar/weeklymood.js
@@ -8,7 +8,8 @@
 var trace = {
   y: [3, 2, 2, 5, 5, 5],
   x: [16, 17, 19, 20, 21, 22],
-  type: 'scatter'
+  type: 'scatter',
+  name: 'Mood Level'
 };
 
 var data = [trace];
@@ -16,8 +17,34 @@ var data = [trace];
 
 var layout = {
     // title: 'Hide the Plotly Logo on the Modebar',
-    showlegend: false
-};
+    showlegend: true,
+    title: {
+      text:'Mood over the course of the week',
+      font: {
+        size: 24
+      },
+      xref: 'paper',
+      x: 0.05,
+    },
+    xaxis: {
+      title: {
+        text: 'Days of the Week',
+        font: {
+          size: 18,
+          color: '#7f7f7f'
+        }
+      },
+    },
+    yaxis: {
+      title: {
+        text: 'Mood Rating (1 being sad and 5 being happy',
+        font: {
+          size: 18,
+          color: '#7f7f7f'
+        }
+      }
+    }
+  };
 
 Plotly.newPlot('myDiv', data, layout, {displaylogo: false});
 
